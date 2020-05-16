4df8114a7483ff927777ae6c934cc6df0b635687
Андрій Лесів <aner98lam@ukr.net>
2020-05-16 14:18:53 +0300

    Rename directory

--- ClientApp/src/app/app.component.spec.ts
+++ ClientApp/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'ClientApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular client');
+    expect(app.title).toEqual('ClientApp');
   });
 
   it('should render title', () => {
